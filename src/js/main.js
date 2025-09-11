8c6b271b6a82f989ac73d7e1d30eaf93ed560ccf
신 수정 <jhw02045@naver.com>
2025-09-11 15:07:07 +0900

    [2025.09.11]

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,11 @@
  * 1. 헤더에 오늘 날짜 불러오기 o
  * 2. 할 일 등록 하는 입력 창 o
  * 3. list 목록 보여지게 o
- * 4. 완료 버튼 클릭 시 선으로 긋기
- * 5. 완료된 목록은 아래로 가도록 하기
- * 6. 할 일 삭제 버튼
- * 7. 할 일 수정 버튼
+ * 4. 할 일 완료 버튼 o
+ * 5. 할 일 삭제 버튼
+ * 6. 완료 버튼 클릭 시 선으로 긋기
+ * 7. 완료된 목록은 아래로 가도록 하기
+ * 8. 할 일 수정 버튼
  */
 
 const getDate = new Date();
@@ -22,12 +23,23 @@ window.onload = function(){
 
     document.getElementById("add-button").addEventListener('click', function() {
 
-        const todoList = document.getElementById("input-box").value;
+        let todoList = document.getElementById("input-box").value;
 
         const txt_todoList = document.createTextNode(todoList);
-        const todoLists = document.createElement("p");
+        const todoLists = document.createElement("li");
+        const completeButtons = document.createElement("button");
+
+        completeButtons.addEventListener('click', function () {
+            todoLists.style.textDecoration = "line-through"
+            this.textContent = "완료됨"
+        });
+        completeButtons.style.marginLeft = '10px';
+        completeButtons.textContent="완료";
+
+
 
         todoLists.appendChild(txt_todoList);
+        todoLists.appendChild(completeButtons);
 
         document.getElementById("addTodo").appendChild(todoLists);
         document.getElementById("input-box").value = "";
